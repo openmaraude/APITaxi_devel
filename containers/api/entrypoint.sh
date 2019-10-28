6434bc58d3a6b92abff3dfe09f02145ed5af8df3
Julien Castets <castets.j@gmail.com>
2019-10-28 14:53:20 +0100

    Dockerize worker

--- containers/api/entrypoint.sh
+++ containers/api/entrypoint.sh
@@ -2,11 +2,13 @@
 
 # If the directory /venv/ is empty, fill it with a new virtualenv. /venv can be
 # mounted as a docker volume to persist packages installation.
-find /venv/ -maxdepth 0 -empty -exec virtualenv /venv \;
+sudo -E find /venv/ -maxdepth 0 -empty -exec virtualenv /venv \;
 
-pip3 install -e /git/APITaxi_utils
-pip3 install -e /git/APITaxi_models
-pip3 install -e /git/APITaxi
+. /venv/bin/activate
+
+sudo -E /venv/bin/pip install -e /git/APITaxi_utils
+sudo -E /venv/bin/pip install -e /git/APITaxi_models
+sudo -E /venv/bin/pip install -e /git/APITaxi
 
 # Execute Docker CMD
 exec "$@"
