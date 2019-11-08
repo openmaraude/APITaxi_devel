781de663c6f02e9023d09292e71663873bc821b7
Julien Castets <castets.j@gmail.com>
2019-11-08 15:41:18 +0100

    Add geotaxi container

---
+++ containers/geotaxi/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+kill_running_server() {
+    pkill --signal SIGKILL geoloc-server
+}
+
+run_geoloc_server() {
+    kill_running_server
+    wait-for-it redis:6379 -- ./geoloc-server -p 8080 --redisurl redis &
+}
+
+cd /geotaxi
+
+# Kill server on ctrl+c
+trap 'kill_running_server' TERM INT
+
+# Run server
+make
+run_geoloc_server
+
+# On change, recompile and run server
+while inotifywait -o /dev/null -r --exclude '.*git.*' /geotaxi 2>/dev/null
+do
+    echo "Changes detected, recompile and restart server" >&2
+    make
+    run_geoloc_server
+done
