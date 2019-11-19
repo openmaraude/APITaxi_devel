76a90676b7b616f1080954ff63981004255b9850
Julien Castets <castets.j@gmail.com>
2019-11-19 12:47:01 +0100

    Add container for minimal-operateur-server

---
+++ containers/minimal-operateur-server/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# If the directory /venv/ is empty, fill it with a new virtualenv. /venv can be
+# mounted as a docker volume to persist packages installation.
+sudo -E find /venv/ -maxdepth 0 -empty -exec virtualenv /venv \;
+
+. /venv/bin/activate
+
+sudo -E /venv/bin/pip install -e "/git/minimal_operateur_server"
+
+# Execute Docker CMD
+exec "$@"
