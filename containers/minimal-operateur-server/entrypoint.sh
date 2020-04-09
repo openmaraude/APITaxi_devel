94af44bab745d8415a9e720f861811bb4c48eb6d
Julien Castets <castets.j@gmail.com>
2020-04-09 18:12:36 +0200

    Install minimal-operateur-server with latest version

--- containers/minimal-operateur-server/entrypoint.sh
+++ containers/minimal-operateur-server/entrypoint.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
-# If the directory /venv/ is empty, fill it with a new virtualenv. /venv can be
-# mounted as a docker volume to persist packages installation.
-sudo -E find /venv/ -maxdepth 0 -empty -exec virtualenv /venv \;
+set -e
 
-. /venv/bin/activate
+# If settings already exists, execute CMD
+test -f /settings/settings.py && exec "$@"
 
-sudo -E /venv/bin/pip install -e "/git/minimal_operateur_server"
+# Otherwise, install PostgreSQL and get the apikey from user "neotaxi".
+apt-get update -q
+apt-get install -qqqy postgresql-client
 
-# Execute Docker CMD
+API_KEY=$(psql -h db -U postgres taxis -t -c "SELECT apikey FROM \"user\" WHERE email = 'neotaxi'" | xargs)
+
+if [ "$API_KEY" = "" ];
+then
+    echo "Unable to get apikey for user neotaxi" >&2
+    exit 1
+fi
+
+cat<<EOF > /settings/settings.py
+RQ_REDIS_URL = 'redis://redis:6379/0'
+API_TAXI_URL = 'http://api:5000'
+API_TAXI_KEY = '${API_KEY}'
+EOF
+
 exec "$@"
