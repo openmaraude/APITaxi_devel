06650ebedf9b83921d38112a8a6e24f470bd4cc1
Julien Castets <castets.j@gmail.com>
2020-10-14 15:09:12 +0200

    Install pytest and remove APITaxi_utils

--- containers/api/entrypoint.sh
+++ containers/api/entrypoint.sh
@@ -6,9 +6,9 @@ sudo -E find /venv/ -maxdepth 0 -empty -exec virtualenv /venv \;
 
 . /venv/bin/activate
 
-sudo -E /venv/bin/pip install flower tox watchdog[watchmedo]
+sudo -E /venv/bin/pip install flower tox watchdog[watchmedo] pytest
 
-for proj in APITaxi_utils APITaxi_models APITaxi APITaxi_front;
+for proj in APITaxi_models APITaxi APITaxi_front;
 do
     test -d "/git/${proj}" && sudo -E /venv/bin/pip install -e "/git/${proj}"
 done
