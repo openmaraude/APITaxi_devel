5bb177535b2fab8e857ea3017b0a33b54a84643c
Julien Castets <castets.j@gmail.com>
2021-05-03 11:50:36 +0200

    api: fix reference to undefined variable in entrypoint

--- containers/api/entrypoint.sh
+++ containers/api/entrypoint.sh
@@ -8,7 +8,7 @@ sudo -E find /venv/ -maxdepth 0 -empty -exec virtualenv /venv \;
 
 sudo -E /venv/bin/pip install flower tox watchdog[watchmedo] pytest flake8 pylint
 
-test -d "/git/APITaxi" && sudo -E /venv/bin/pip install -e "/git/${proj}"
+test -d "/git/APITaxi" && sudo -E /venv/bin/pip install -e "/git/APITaxi"
 
 # Execute Docker CMD
 exec "$@"
